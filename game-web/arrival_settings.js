3f09989814d8f6471fd03ef8630d6c584b92606c
Map Editor <allenkao@dila.edu.tw>
2026-08-26 15:58:19 +0800

    部署 v2.32

--- game-web/arrival_settings.js
+++ game-web/arrival_settings.js
@@ -3,57 +3,100 @@
 window.ARRIVAL_SETTINGS = {
   "kids": [
     {
-      "img": 7,
-      "left": 10,
-      "bottom": 15,
-      "width": 30
-    },
-    {
       "img": 0,
-      "left": -2,
-      "bottom": 8,
-      "width": 18
+      "left": 0,
+      "bottom": 14,
+      "width": 14
     },
     {
       "img": 1,
       "left": 8,
-      "bottom": 8,
-      "width": 18
+      "bottom": 13,
+      "width": 14
     },
     {
       "img": 2,
-      "left": -7,
-      "bottom": -5,
-      "width": 19
+      "left": 16,
+      "bottom": 13,
+      "width": 14
     },
     {
       "img": 3,
       "left": 24,
-      "bottom": 4,
-      "width": 19
+      "bottom": 13,
+      "width": 14
     },
     {
       "img": 4,
-      "left": 32,
+      "left": -1,
       "bottom": -5,
-      "width": 18
+      "width": 17
     },
     {
       "img": 5,
-      "left": 17,
+      "left": 8,
       "bottom": -5,
-      "width": 18
+      "width": 17
     },
     {
       "img": 6,
-      "left": 4,
+      "left": 17,
       "bottom": -5,
-      "width": 18
+      "width": 17
+    },
+    {
+      "img": 7,
+      "left": 26,
+      "bottom": -5,
+      "width": 17
     }
   ],
   "title": {
     "top": 25,
     "left": 73,
     "scale": 1
+  },
+  "trains": {
+    "local": {
+      "image": "../圖片/慶祝到站列車/劇場版雙節v0.1/慢車-雙節.png",
+      "width": 160,
+      "stopRight": -85,
+      "bottom": -40,
+      "arrivalDuration": 6,
+      "exitRight": 115,
+      "departureDuration": 4.8
+    },
+    "express": {
+      "image": "../圖片/慶祝到站列車/劇場版雙節v0.1/快車-雙節.png",
+      "width": 235,
+      "stopRight": -170,
+      "bottom": -70,
+      "arrivalDuration": 4.7,
+      "exitRight": 115,
+      "departureDuration": 3.8
+    },
+    "tzechiang": {
+      "image": "../圖片/慶祝到站列車/劇場版雙節v0.1/普悠瑪-雙節.png",
+      "width": 245,
+      "stopRight": -170,
+      "bottom": -75,
+      "arrivalDuration": 3.8,
+      "exitRight": 115,
+      "departureDuration": 3.2
+    },
+    "hsr": {
+      "image": "../圖片/慶祝到站列車/劇場版雙節v0.1/高鐵-雙節.png",
+      "width": 235,
+      "stopRight": -165,
+      "bottom": -50,
+      "arrivalDuration": 2.9,
+      "exitRight": 115,
+      "departureDuration": 2.7
+    }
+  },
+  "flow": {
+    "promptDelay": 3650,
+    "nextRevealDelay": 500,
+    "departurePromptBuffer": 250
   }
 };
